fbe06cec32fa799fe968b8425cc1521ef76dd729
werlyana <werlyanabruno@gmail.com>
2022-09-07 19:08:50 -0300

    Aula 01 e 02

---
+++ Aula01_CalculadoraDeMedia/script.js
@@ -0,0 +1,37 @@
+var nome = "Guilherme"
+
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 7
+var notaDoTerceiroBimestre = 4
+var notaDoQuartoBimestre = 2 
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem vindo " + nome)
+console.log(notaFixada)
+
+
+
+
+
+//var nome = prompt("Qual é seu nome?")
+//comando de entrada prompt
+//var nota1 = parseFloat(prompt("Informe sua nota do Primeiro bimestre: "));
+//var nota2 = parseFloat(prompt("Informe sua nota do Segundo bimestre: "))
+//var nota3 = parseFloat(prompt("Informe sua nota do Terceiro bimestre: "))
+//var nota4 = parseFloat(prompt("Informe sua nota do Quarto bimestre: "))
+//parseFloat para converter em número real
+//var notaFinal = (nota1 + nota2 + nota3 + nota4) / 4
+
+//console.log("Olá " + nome)
+//se soma da nota final for maior ou igual a 6, o aluno é aprovado, senão, reprovado.
+  //if (notaFinal>=6)
+    //console.log("Parabéns, você foi aprovado! Nota Final de: " + notaFinal.toFixed(1))
+ //tofixed passa o número de casa decimal que quero fixar   
+  //else 
+  //console.log("Ops, você foi reprovado. Nota Final de: " + notaFinal.toFixed(1))
+    //
+
+  //
