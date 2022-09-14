7579ceb2c5eb6319cb49e34030754f9f550de160
werlyana <werlyanabruno@gmail.com>
2022-09-14 20:13:53 -0300

    Incluindo url dos filmes

---
+++ Aula04_Array_Lista_AluraFlix/script.js
@@ -0,0 +1,17 @@
+
+// a url da imagem que aparecerá na tela
+
+var listaFilmes = [
+    "https://br.web.img2.acsta.net/pictures/19/11/26/09/09/5380589.jpg",
+    "https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/91/90/98/20169244.jpg" , "https://upload.wikimedia.org/wikipedia/pt/9/9a/Big-bang-theory_Temporada_12_poster.jpg" , "https://i.pinimg.com/564x/00/ea/7c/00ea7c88e9e80f8bea6d13f3f7f0a810.jpg" , "https://realnerd.com.br/wp-content/uploads/2021/03/MV5BODM1OTk4MDY2M15BMl5BanBnXkFtZTgwOTEwNzczMDI@._V1_.jpg" , "https://feira.tangerina.uol.com.br/wp-content/uploads/2022/09/cobra-kai-5-temporada-cartaz-netflix.jpg" , "https://br.web.img3.acsta.net/pictures/18/12/31/02/49/2373131.jpg" , "https://br.web.img3.acsta.net/medias/nmedia/18/97/52/82/20534159.jpg" , "https://s2.glbimg.com/LGbXABbLjhAFyykClb_ZJHtl9Ys=/362x536/https://s2.glbimg.com/wIP8E6G5-eO6a1IvSzMIPIG7vTk=/i.s3.glbimg.com/v1/AUTH_c3c606ff68e7478091d1ca496f9c5625/internal_photos/bs/2022/F/x/t2KjGoTDAGQCBpjgoMsQ/2022-2701-the-good-doctor-poster-t5.jpg" , "https://images-na.ssl-images-amazon.com/images/I/81QE7xH-thL.jpg" , "https://br.web.img2.acsta.net/pictures/19/06/11/21/33/0075880.jpg" , "http://www.adirferreira.com.br/wp-content/uploads/2016/03/how-to-get-away-witu-murder-poster.jpg"
+];
+
+//    valor inicial.   condição. expressão final
+
+for (var i = 0; i < listaFilmes.length; i++) {
+document.write("<img src=" + listaFilmes[i] + ">");
+}
+
+//var listaFilmes = ['Yesterday', 'A chegada', 'Escola de Rock'];
+//document.write("<img src=" + listaFilmes[i] + ">");}
+
